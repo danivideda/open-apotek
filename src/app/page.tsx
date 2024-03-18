0f36044518cf95e80f34c44cbf927886cf110dcb
Dani Ihza Farrosi <dani.videda@gmail.com>
2024-03-18 21:35:17 +0700

    login form, WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,24 @@ export default function Home() {
     <div className="h-[100vh] bg-gradient-to-r from-[#C1DCC6] to-[#E1F0E6]">
       <div className="flex justify-center items-center h-full w-full">
         <div className="flex flex-col justify-center items-center w-1/2 h-3/4 rounded-xl shadow-md bg-[#FBFFFB]">
-          <div className="w-96 h-16 border border-black">username</div>
-          <div className="w-96 h-16 border border-black">username</div>
+          <form action="">
+            <label htmlFor="username" className="block">
+              username
+            </label>
+            <input
+              type="text"
+              name="username"
+              className="border border-gray-300 rounded-md"
+            />
+            <label htmlFor="username" className="block">
+              password
+            </label>
+            <input
+              type="password"
+              name="password"
+              className="border border-gray-300 rounded-md"
+            />
+          </form>
         </div>
       </div>
     </div>
