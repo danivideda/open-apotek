de2c47f9b6827993aa5d8f44a9d6214041a1b495
Dani Ihza Farrosi <dani.videda@gmail.com>
2024-04-22 11:42:30 +0700

    finish implement login session in middleware with sessionToken cookies

--- src/middleware.ts
+++ src/middleware.ts
@@ -43,39 +43,46 @@ export async function middleware(request: NextRequest) {
     }
   }
 
-  // if (request.nextUrl.pathname.startsWith("/dashboard")) {
-  //   const sessionToken = request.cookies.get(
-  //     sessionTokenCookieConfig().name
-  //   )?.value;
-  //   if (!sessionToken) {
-  //     const url = request.nextUrl.clone();
-  //     url.pathname = "/login";
+  if (request.nextUrl.pathname.startsWith("/dashboard")) {
+    const sessionToken = request.cookies.get(
+      sessionTokenCookieConfig().name
+    )?.value;
+    if (!sessionToken) {
+      const url = request.nextUrl.clone();
+      url.pathname = "/login";
 
-  //     return NextResponse.redirect(url);
-  //   }
+      return NextResponse.redirect(url);
+    }
 
-  //   try {
-  //     await jose.jwtVerify(
-  //       sessionToken,
-  //       new TextEncoder().encode(process.env.JWT_REFRESH_SECRET!)
-  //     );
+    try {
+      await jose.jwtVerify(
+        sessionToken,
+        new TextEncoder().encode(process.env.JWT_REFRESH_SECRET!)
+      );
 
-  //     const urlApi = `http://${request.nextUrl.host}/api/auth/check`;
-  //     const response = await fetch(urlApi, { method: "GET" });
-  //     if (response.status === ResponseStatus.BadRequest) {
-  //       throw new Error("Check sessionToken in DB doesn't match");
-  //     }
+      const urlFetch = `http://${request.nextUrl.host}/api/auth/check`;
+      const response = await fetch(urlFetch, {
+        method: "GET",
+        headers: {
+          Cookie: `${sessionTokenCookieConfig().name}=${sessionToken}`,
+        },
+      });
 
-  //     return NextResponse.next();
-  //   } catch (error) {
-  //     console.log(error);
+      if (response.status === ResponseStatus.BadRequest) {
+        const body = await response.json();
+        throw new Error(body.message);
+      }
 
-  //     const url = request.nextUrl.clone();
-  //     url.pathname = "/login";
+      return NextResponse.next();
+    } catch (error) {
+      console.log(error);
 
-  //     return NextResponse.redirect(url);
-  //   }
-  // }
+      const url = request.nextUrl.clone();
+      url.pathname = "/login";
+
+      return NextResponse.redirect(url);
+    }
+  }
 }
 
 export const config = {
